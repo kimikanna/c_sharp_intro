c3d6c4677f4d83d9f0602d98a48b3877788c129f
kimikanna <kannapog@gmail.com>
2023-08-12 00:47:05 +0500

    Completed HW Task 8

---
+++ HW1/Task8/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int j = -1; 
+
+for (int i = 2; i < number - 1; i += 2)
+{
+    Console.Write($"{i}, ");
+    j = i;
+}
+
+if ( j == -1)
+{
+    Console.WriteLine("Чётных чисел от 1 до N не существует");
+}
+else
+{
+    j += 2;
+    Console.WriteLine(j);
+}
