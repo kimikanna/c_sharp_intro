858469d4d27d597ebf5bb325e2ead4b71567c80c
kimikanna <kannapog@gmail.com>
2023-08-11 16:31:53 +0500

    Completed HW Task 2

---
+++ HW1/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+int max = num1;
+
+if (num2 > num1)
+{
+    max = num2;
+}
+
+Console.WriteLine($"Наибольшее число из двух введённых: {max}");
