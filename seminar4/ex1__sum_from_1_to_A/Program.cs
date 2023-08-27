4b3d88aada0ed172a9e07243a7c72eedd9c5656c
kimikanna <kannapog@gmail.com>
2023-08-27 12:55:53 +0500

    Added sem4

---
+++ seminar4/ex1__sum_from_1_to_A/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int inputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SumFrom1ToNum(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+        sum += i;
+    return sum;
+}
+
+int number = inputNum("Введите число: ");
+int sum = SumFrom1ToNum(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} равна: {sum}");
---
+++ seminar4/ex2__num_of_digits/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int inputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int NumOfDigits(int num)
+{
+    int digitCount = 0;
+    if (num == 0)
+        return digitCount;
+    digitCount = (int)Math.Log10(num) + 1;
+    return digitCount;
+}
+
+int number = inputNum("Введите число: ");
+int amount = NumOfDigits(number);
+Console.WriteLine($"Количество цифр в вашем числе: {amount}");
