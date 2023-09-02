6fee66959340b1d70cb0e3aea58cfc79b43c823d
kimikanna <kannapog@gmail.com>
2023-09-03 00:17:07 +0500

    Solved Task 27

---
+++ HW4/Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SumOfDigits(int num)
+{
+    int sum = 0;
+    for (int i = 0; num != 0; i++)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return sum;
+}
+
+int num = InputNum("Введите целое число: ");
+int result = SumOfDigits(num);
+Console.WriteLine($"Сумма всех цифр введённого числа: {result}");
