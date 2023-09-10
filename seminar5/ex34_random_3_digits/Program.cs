e0f7e413a09f8ddff58b6cde6e607f1475324de3
kimikanna <kannapog@gmail.com>
2023-09-10 18:55:16 +0500

    Solved Task 34

---
+++ seminar5/ex34_random_3_digits/Program.cs
@@ -0,0 +1,62 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int CheckZero(int size)
+{
+    while (size <= 0)
+    {
+        int correctSize = InputNum("Введите размер массива больше нуля: ");
+        size = correctSize;
+    }
+
+    return size;
+}
+
+int[] ThreeDigits(int size)
+{
+    int[] array = new int[size];
+    Random element = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = element.Next(100, 1000);
+    }
+
+    return array;
+}
+
+int NumOfEven(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            counter++;
+    }
+
+    return counter;
+}
+
+string PrintArray(int[] array)
+{
+    string arr = "[ ";
+    for (int i = 0; i < array.Length - 1; i++)
+        arr += array[i] + ", ";
+
+    arr += array[array.Length - 1] + " ]";
+    return arr;
+}
+
+int size = InputNum("Введите количество элементов в массиве: ");
+size = CheckZero(size);
+int[] array = ThreeDigits(size);
+int counter = NumOfEven(array);
+string arr = PrintArray(array);
+Console.WriteLine(arr);
+Console.WriteLine($"Количество чётных элементов: {counter}");
